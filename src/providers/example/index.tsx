f77d0e859f81823888066ae294a153f3497997dc
l1b3r_- <liber@ricardojrm.com>
2021-12-23 13:36:35 +0000

    perf: bootstrap with supernova

---
+++ src/providers/example/index.tsx
@@ -0,0 +1,28 @@
+import React, { useMemo } from 'react';
+import { ExampleContext, ExampleContextType } from '../../contexts/example';
+
+/**
+ * ExampleProviderProps
+ */
+export interface ExampleProviderProps {
+  /**
+   * Prop
+   */
+  children: JSX.Element;
+}
+
+/**
+ * ExampleProvider
+ */
+export const ExampleProvider = ({ children }: ExampleProviderProps) => {
+  // eslint-disable-next-line arrow-body-style
+  const ctx = useMemo<ExampleContextType>(() => {
+    return {
+      example: 'example',
+    };
+  }, []);
+
+  return (
+    <ExampleContext.Provider value={ctx}>{children}</ExampleContext.Provider>
+  );
+};
